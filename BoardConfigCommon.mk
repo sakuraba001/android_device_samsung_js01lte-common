LineageOS/android_device_samsung_hlte-common

Hlte-common: add common ril
Change-Id: If0d0530638d09e6de16f7978baa8bb999f2be274

BoardConfigCommon.mk
@@ -53,7 +53,8 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # CMHW
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
 BOARD_HARDWARE_CLASS += device/samsung/hlte-common/cmhw
-
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/hlte-common/ril
 # Graphics
 TARGET_HAVE_NEW_GRALLOC := true
 BOARD_USES_LEGACY_MMAP := true

sakuraba001/android_device_samsung_js01lte-common

BoardConfigCommon.mk
@@ -54,6 +54,8 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
 BOARD_HARDWARE_CLASS += device/samsung/js01lte-common/cmhw
 
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/js01lte-common/ril
 # Graphics
 TARGET_HAVE_NEW_GRALLOC := true
 BOARD_USES_LEGACY_MMAP := true